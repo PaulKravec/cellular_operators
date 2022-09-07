87f9ccffe090fc478f70a2e6b9b94ac045e902f0
Paul_RazDva <paul1312white@gmail.com>
2022-09-08 00:33:55 +0300

    gitignore

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 100; // Начальный счёт
+        int x1 = 1100; // Cумма пополнения
+        int x2 = 100; // Вторая сумма пополнения
+        if (x1 >= 1000) {
+            System.out.println(x1 + x + (x1 / x));
+        } else {
+            System.out.println(x + x2);
+        }
+
+
+    }
+}
